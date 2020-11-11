68f3b60a5fc19032d7d40037b63dbaa997194bb3
sourav.ravish <sourav.ravish@fireeye.com>
2020-11-11 20:24:33 +0530

    Updated code

---
+++ web-server/challenges/12.js
@@ -0,0 +1,5 @@
+// Goal: Create a partial for footer
+
+// 1. Setup the template for thr footer partial "Created by <your name>"
+// 2. Render the partial at the bottom of all 3 pages
+// 3. Test your work by visitng all 3 pages.
---
+++ web-server/challenges/13.js
@@ -0,0 +1,10 @@
+// Goal: Create and render an actual 404 page with handlebars
+
+// 1. Setup the template to render the header and footer
+// 2. Setup the template to render an error message in a paragraph
+// 3. Render the template for both 404 routes
+//    - Page not found
+//    - Help article not found
+// 4. Test your work. Visit /what and /help/units
+
+// Hint: Create only 1 single hbs file taking in Error as one of data values from var.
