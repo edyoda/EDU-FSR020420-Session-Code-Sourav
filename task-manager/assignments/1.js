ea0d257277353875fda5efb67cccb2ce1c821797
sourav.ravish <sourav.ravish@fireeye.com>
2020-11-24 20:29:59 +0530

    take-home assignment

---
+++ task-manager/assignments/1.js
@@ -0,0 +1,10 @@
+// Assignment
+// Install MongoDB and GUI Viewer Robo 3T
+
+// Goal: Insert 3 tasks into a new tasks collection
+
+// 1. Use insertMany to insert 3 documents
+//    -- description(string), completed(boolean)    
+// 2. Setup the callback to handler error or print ops
+// 3. Run the script
+// 4. Refresh the database in Robo 3t and view data in tasks collection
