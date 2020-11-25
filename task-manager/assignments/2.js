3b01af744bb15854eefa5f8b7d1840238ede2da3
sourav.ravish <sourav.ravish@fireeye.com>
2020-11-25 20:27:23 +0530

    assigbment2

---
+++ task-manager/assignments/2.js
@@ -0,0 +1,5 @@
+// Goal: Use find and findOne with tasks
+
+// 1. Use findOne to fetch the last task by its id
+// 2. Use find to fetch all tasks that are not completed (print docs to console)
+// 3. Test your work!
